1111ddc057e452ce9e7aa4661ca8b81e23ca17ab
amira <abidoamira@gmail.com>
2025-10-01 12:55:33 +0300

    new demo 5.2

--- 5-2-react-starter/src/App.jsx
+++ 5-2-react-starter/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import './components/StudentCard'
+import StudentCard from './components/StudentCard';
 function App() {
   return (
     <div className="app">
@@ -11,7 +11,7 @@ function App() {
       <main className="dashboard-main">
         <div className="cards-container">
           {/* TODO: Import and render StudentCard components here */}
-            <StudentCard name="Amira" id="202248560" dept="computerscience "/>
+            <StudentCard name="Amira Abido" id="202248560" dept="computerscience "/>
             <StudentCard name="Mai Hammad" id="202245670" dept="comp senice "/>
         </div>
       </main>
--- 5-2-react-starter/src/components/StudentCard.jsx
+++ 5-2-react-starter/src/components/StudentCard.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function StudentCard(props) {
     return (
         <div>
